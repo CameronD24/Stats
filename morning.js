120bf893270dc0adda4e13c5aa1118171b364d9d
Cameron Dixon <cam@camerons-mbp.mynetworksettings.com>
2023-10-17 21:41:31 -0400

    Add

---
+++ morning.js
@@ -0,0 +1,12 @@
+function sayHello(){
+console.log('Hello')
+}
+
+function sayHelloTo( name ) {
+  console.log('Hello,', name)
+}
+sayHelloTo('Cam')
+
+const myInstruments = ['saxophone', 'flute', 'clarinet', 'trumpet', 'trombone', 'tuba', 'bassoon'];
+myInstruments.push('guitar')
+console.log(myInstruments)
